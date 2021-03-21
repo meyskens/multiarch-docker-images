8b47525e5466290b638740d6bb6ed7e8ca0776ce
Maartje Eyskens <maartje@kinvolk.io>
2021-03-21 14:30:19 +0100

    Set platforms

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ "$2" == ""]; then
-    echo "Usage build.sh <repo> <target> <path in repo>"
+if [ "$3" == ""]; then
+    echo "Usage build.sh <repo> <target> <platforms> <path in repo>"
     exit 1
 fi
 
@@ -10,16 +10,16 @@ TEMP=$(mktemp -d)
 cd $TEMP
 git clone $1 ./
 
-if [ "$3" != ""]; then
-    cd "$3"
+if [ "$4" != ""]; then
+    cd "$4"
 fi
 
 awk '/FROM/ { print; print "LABEL org.opencontainers.image.source=https://github.com/meyskens/multiarch-docker-images"; next }1' Dockerfile
 
-docker buildx build --platform "linux/amd64,linux/arm64,linux/arm" -t "$2:main" --push .
+docker buildx build --platform "$3" -t "$2:main" --push .
 
 if [ "$(git tag -n)" != "" ]; then
     TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
     git checkout $TAG
-    docker buildx build --platform "linux/amd64,linux/arm64,linux/arm" -t "$2:$TAG" --push .
+    docker buildx build --platform "$3" -t "$2:$TAG" --push .
 f
--- generate.sh
+++ generate.sh
@@ -3,12 +3,13 @@
 mkdir -p .github/workflows/
 rm -f .github/workflows/*
 
-while IFS=, read -r repo path target; do
+while IFS=, read -r repo path target platforms; do
   if [[ "$repo" != "repo" ]]; then 
     NAME=$(echo "$target" | tr '/' '\n' | tail -n1)
     cp template.yaml .github/workflows/$NAME.yaml
     sed -i 's#$repo#'$repo'#g' .github/workflows/$NAME.yaml
     sed -i 's#$target#'$target'#g' .github/workflows/$NAME.yaml
     sed -i 's#$path#'$path'#g' .github/workflows/$NAME.yaml
+    sed -i 's#$platforms#'$platforms'#g' .github/workflows/$NAME.yaml
   fi
 done < image-list
