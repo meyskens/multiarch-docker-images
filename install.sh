86686e9b31b9dd7d1d7ec0a3538d01e29f61f398
Maartje Eyskens <maartje@eyskens.me>
2021-03-26 20:02:31 +0100

    Fix BUILDX_URL

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ sudo apt update
 sudo apt-get install -y binfmt-support qemu-user-static jq
 
 mkdir -p ~/.docker/cli-plugins
-BUILDX_URL=$(curl https://api.github.com/repos/docker/buildx/releases/latest | jq -r .assets[].browser_download_url | grep amd64)
+BUILDX_URL=$(curl https://api.github.com/repos/docker/buildx/releases/latest | jq -r .assets[].browser_download_url | grep amd64 | grep linux)
 wget $BUILDX_URL -O ~/.docker/cli-plugins/docker-build
 chmod +x ~/.docker/cli-plugins/docker-buildx
 
