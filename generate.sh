b25e646a480d1addc916585b4ad772d83b8204cd
Maartje Eyskens <maartje@eyskens.me>
2021-03-28 13:58:37 +0200

    Shorter build job names
    Signed-off-by: Maartje Eyskens <maartje@eyskens.me>

--- generate.sh
+++ generate.sh
@@ -7,6 +7,7 @@ while IFS="," read -r repo path target platforms tagfilter; do
   if [[ "$repo" != "repo" ]]; then 
     NAME=$(echo "$target" | tr '/' '\n' | tail -n1)
     cp template.yaml .github/workflows/$NAME.yaml
+    sed -i 's#$name#'$NAME'#g' .github/workflows/$NAME.yaml
     sed -i 's#$repo#'$repo'#g' .github/workflows/$NAME.yaml
     sed -i 's#$target#'$target'#g' .github/workflows/$NAME.yaml
     sed -i 's#$path#'$path'#g' .github/workflows/$NAME.yaml
