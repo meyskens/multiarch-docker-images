f23af65c00918ec4258d0db3a59b49d7c19c3c80
Maartje Eyskens <maartje@eyskens.me>
2021-03-28 13:56:38 +0200

    Support Bitnamis weird repo structure
    Signed-off-by: Maartje Eyskens <maartje@eyskens.me>

--- generate.sh
+++ generate.sh
@@ -3,7 +3,7 @@
 mkdir -p .github/workflows/
 rm -f .github/workflows/*
 
-while IFS=, read -r repo path target platforms; do
+while IFS="," read -r repo path target platforms tagfilter; do
   if [[ "$repo" != "repo" ]]; then 
     NAME=$(echo "$target" | tr '/' '\n' | tail -n1)
     cp template.yaml .github/workflows/$NAME.yaml
@@ -11,5 +11,6 @@ while IFS=, read -r repo path target platforms; do
     sed -i 's#$target#'$target'#g' .github/workflows/$NAME.yaml
     sed -i 's#$path#'$path'#g' .github/workflows/$NAME.yaml
     sed -i 's#$platforms#'$platforms'#g' .github/workflows/$NAME.yaml
+    sed -i 's#$tagfilter#'$tagfilter'#g' .github/workflows/$NAME.yaml
   fi
 done < image-list
